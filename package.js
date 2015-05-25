4b21adee8d4b6b518cb6dbeaa63c64350bd148f2
Le Thanh <particle4dev01@gmail.com>
2015-05-25 11:25:30 +0700

    release 1.1.0: add share via email

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  name: 'lethanh:social-sharing',
-  version: '1.0.0',
-  summary: 'The plugin manages share text, a file, or a URL (or all three) via the native sharing widget',
-  git: 'git@github.com:thanhlvbk/social-sharing.git',
-  documentation: null
+    name: 'lethanh:social-sharing',
+    version: '1.1.0',
+    summary: 'The plugin manages share text, a file, or a URL (or all three) via the native sharing widget',
+    git: 'git@github.com:thanhlvbk/social-sharing.git',
+    documentation: null
 });
 
 Cordova.depends({
@@ -11,15 +11,15 @@ Cordova.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('particle4dev:rsvpjs@3.0.18', 'client');
-  api.addFiles('social-sharing.js', 'web.cordova');
-  if (api.export)
+    api.versionsFrom('1.0');
+    api.use('particle4dev:rsvpjs@3.0.18', 'client');
+    api.addFiles('social-sharing.js', 'web.cordova');
+    if (api.export)
         api.export('SOCIAL_SHARING', 'web.cordova');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('lethanh:social-sharing');
-  api.addFiles('social-sharing-tests.js');
+    api.use('tinytest');
+    api.use('lethanh:social-sharing');
+    api.addFiles('social-sharing-tests.js');
 });
